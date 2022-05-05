37d86908ac11f06dd82d8a5939f3ed1e4c12720e
LeoChenot <leochenot1@gmail.com>
2022-05-05 17:42:38 +0200

    Menu v1

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
+import Menu from '../Menu';
 import './style.scss';
 
 function App() {
   return (
     <div className="App">
       <p>Header</p>
+      <Menu />
       <p>Main</p>
       <p>Footer</p>
     </div>
