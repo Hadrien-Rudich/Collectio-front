5676a13a19d059ff211770bb7037e38dbb7d529c
LeoChenot <leochenot1@gmail.com>
2022-05-05 15:03:44 +0200

    Installation of dependencies and creation of store and reducers redux

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Header</p>
+      <p>Main</p>
+      <p>Footer</p>
+    </div>
+  );
+}
+
+export default App;
