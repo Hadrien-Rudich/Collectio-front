0ea4c5a37eb4dc3b5ae40f7b4e91f1d2051c7c8d
Louis-Cyrus Sanjabi <87710724+Louis-Cyrus@users.noreply.github.com>
2022-05-05 20:50:48 +0200

    addition of style of the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import './style.scss';
 import { GiHamburgerMenu, GiMagnifyingGlass, GiPencil } from 'react-icons/gi';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiTv } from 'react-icons/fi';
+import { MdLocalMovies, MdVideogameAsset } from 'react-icons/md';
+import { ImBooks } from 'react-icons/im';
 
 const Header = () => {
     return(
@@ -10,23 +12,57 @@ const Header = () => {
                 <h1 className='header__title'>Collectio</h1>
             </div>
             <div>
-                <form action='' method='get' className='header__form'>
-                    <select name='filter' id='filter' className='header__filter' multiple>
-                        <option value=''>-- Choose a media --</option>
-                        <option value='movie'>Movie</option>
-                        <option value='serie'>Serie</option>
-                        <option value='book'>Book</option>
-                        <option value='videogames'>Video Games</option>
-                    </select>
-                    <input id='searchbar' type='search' name='search' placeholder='Search media' className='header__searchbar'></input>
-                    <button className='header__look'><GiMagnifyingGlass /></button>
+            <form class="search-form">
+                <input type="search" value="" placeholder="Search" class="search-input"></input>
+                <button type="submit" class="search-button">
+                    <GiMagnifyingGlass/>
+                </button>
+                <div class="search-option">
+                    <div>
+                        <input name="type" type="radio" value="type-users" id="type-users"></input>
+                        <label for="type-users">
+                            <svg class="edit-pen-title">
+                                <MdLocalMovies/>
+                            </svg>
+                            <span>Movies</span>
+                        </label>
+                    </div>
+                    
+                    <div>
+                    <input name="type" type="radio" value="type-posts" id="type-posts"></input>
+                    <label for="type-posts">
+                        <svg class="edit-pen-title">
+                            <FiTv/>
+                        </svg>
+                        <span>Series</span>
+                    </label>
+                    </div>
+                    <div>
+                    <input name="type" type="radio" value="type-images" id="type-images"></input>
+                    <label for="type-images">
+                        <svg class="edit-pen-title">
+                            <ImBooks/>
+                        </svg>
+                        <span>Books</span>
+                    </label>
+                    </div>
+                    <div>
+                    <input name="type" type="radio" value="type-special" id="type-special"></input>
+                    <label for="type-special">
+                        <svg class="edit-pen-title">
+                            <MdVideogameAsset/>
+                        </svg>
+                        <span>VideoGames</span>
+                    </label>
+                    </div>
+                </div>
                 </form>
             </div>
-            <div>
+            <div className='header__user'>
                 <span className='header__login'><FiLogIn />
                     <p>Login</p>
                 </span>
-                <span className='header__login'><GiPencil />
+                <span className='header__register'><GiPencil />
                     <p>Register</p>
                 </span>
             </div>
