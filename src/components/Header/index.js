61ca149f026d546c715aad21ab313fdd6dfc9ae6
Louis-Cyrus Sanjabi <87710724+Louis-Cyrus@users.noreply.github.com>
2022-05-06 12:21:42 +0200

    Change style and dynamisation header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,60 +1,60 @@
 import './style.scss';
+import { Link } from 'react-router-dom';
+
 import { GiHamburgerMenu, GiMagnifyingGlass, GiPencil } from 'react-icons/gi';
 import { FiLogIn, FiTv } from 'react-icons/fi';
 import { MdLocalMovies, MdVideogameAsset } from 'react-icons/md';
 import { ImBooks } from 'react-icons/im';
 
 const Header = () => {
+    const categoriesData = [
+        {
+            name: 'Movies',
+            icon: <MdLocalMovies/>
+        },
+        {
+            name: 'Series',
+            icon: <FiTv/>
+        },
+        {
+            name: 'Books',
+            icon: <ImBooks/>
+        },
+        {
+            name: 'Video Games',
+            icon: <MdVideogameAsset/>
+        }
+    ]
+
     return(
         <header className='header'>
             <div className='header__logo'>
-                <span className='header__burger'><GiHamburgerMenu/></span> 
-                <h1 className='header__title'>Collectio</h1>
+                <span className='header__burger'><GiHamburgerMenu/></span>
+                <Link to="/">
+                   <h1 className='header__title'> Collectio</h1>
+                </Link>
             </div>
             <div>
-            <form class="search-form">
-                <input type="search" value="" placeholder="Search" class="search-input"></input>
-                <button type="submit" class="search-button">
-                    <GiMagnifyingGlass/>
-                </button>
-                <div class="search-option">
+            <form className="header__form">
+                <input type="search" value="" placeholder="Search" className="header__input"></input>
+                <Link to='/results/:searchquery'>
+                    <button type="submit" className="header__button">
+                        <GiMagnifyingGlass/>
+                    </button>
+                </Link>
+                <div className="header__option">
+                {categoriesData.map((category) =>(
+                    
                     <div>
                         <input name="type" type="checkbox" value="type-users" id="type-users"></input>
                         <label for="type-users">
-                            <svg class="edit-pen-title">
-                                <MdLocalMovies/>
+                            <svg className="edit-pen-title">
+                                {category.icon}
                             </svg>
-                            <span>Movies</span>
+                            <span>{category.name}</span>
                         </label>
                     </div>
-                    
-                    <div>
-                    <input name="type" type="checkbox" value="type-posts" id="type-posts"></input>
-                    <label for="type-posts">
-                        <svg class="edit-pen-title">
-                            <FiTv/>
-                        </svg>
-                        <span>Series</span>
-                    </label>
-                    </div>
-                    <div>
-                    <input name="type" type="checkbox" value="type-images" id="type-images"></input>
-                    <label for="type-images">
-                        <svg class="edit-pen-title">
-                            <ImBooks/>
-                        </svg>
-                        <span>Books</span>
-                    </label>
-                    </div>
-                    <div>
-                    <input name="type" type="checkbox" value="type-special" id="type-special"></input>
-                    <label for="type-special">
-                        <svg class="edit-pen-title">
-                            <MdVideogameAsset/>
-                        </svg>
-                        <span>VideoGames</span>
-                    </label>
-                    </div>
+                ))}
                 </div>
                 </form>
             </div>
@@ -62,13 +62,15 @@ const Header = () => {
                 <span className='header__login'><FiLogIn />
                     <p>Login</p>
                 </span>
-                <span className='header__register'><GiPencil />
-                    <p>Register</p>
-                </span>
+
+                <Link to='/register'>
+                    <span className='header__register'><GiPencil />
+                        <p>Register</p>
+                    </span>
+                </Link>
             </div>
         </header>
     );
 }
-
 
 export default Header;
