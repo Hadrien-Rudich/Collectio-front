87aa50b8357b4f14a188ee760e9ab124f2af877e
LeoChenot <leochenot1@gmail.com>
2022-05-11 00:14:33 +0200

    RegisterPage archaic version

--- src/components/MainScreen/index.js
+++ src/components/MainScreen/index.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../HomePage';
+import RegisterPage from '../RegisterPage';
 import './style.scss';
 
 function MainScreen() {
   return (
     <div className="main__screen">
       <Routes>
-        {/* <Route path='/' element={<div>Home</div>} /> */}
         <Route path='/' element={<HomePage />} />
-        <Route path='/register' element={<div>Register</div>} />
+        <Route path='/register' element={<RegisterPage />} />
         <Route path='/movies' element={<div>Movies</div>} />
         <Route path='/series' element={<div>Series</div>} />
         <Route path='/books' element={<div>Books</div>} />
---
+++ src/components/RegisterPage/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+
+import PropTypes from 'prop-types';
+import './style.scss';
+
+function RegisterPage() {
+  return (
+    <div className="registerPage">
+      <form style={{ backgroundColor: '#ccc', padding: '2em' }}>
+        <div style={{ display: 'flex', columnGap: '1em' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="firstname">Firstname</label>
+            <input type="text" id="firstname" />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="lastname">Lastname</label>
+            <input type="text" id="lastname" />
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+        </div>
+        <div style={{ display: 'flex', columnGap: '1em' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="year">Year</label>
+            <input type="number" id="year" />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="month">Month</label>
+            <input type="number" id="month" />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="day">Day</label>
+            <input type="number" id="day" />
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div style={{ display: 'flex', columnGap: '1em' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="password1">Password</label>
+            <input type="text" id="password1" />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label htmlFor="password2">Confirm</label>
+            <input type="text" id="password2" />
+          </div>
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+RegisterPage.propTypes = {
+  
+};
+
+export default RegisterPage;
