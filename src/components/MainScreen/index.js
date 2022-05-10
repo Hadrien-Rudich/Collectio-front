0e309bd43798538d9cd7e842a6b048a4b35717c3
LeoChenot <leochenot1@gmail.com>
2022-05-10 23:44:03 +0200

    GlideJS on homePage

--- src/components/MainScreen/index.js
+++ src/components/MainScreen/index.js
@@ -4,19 +4,18 @@ import HomePage from '../HomePage';
 import './style.scss';
 
 function MainScreen() {
-
   return (
     <div className="main__screen">
       <Routes>
         {/* <Route path='/' element={<div>Home</div>} /> */}
         <Route path='/' element={<HomePage />} />
+        <Route path='/register' element={<div>Register</div>} />
         <Route path='/movies' element={<div>Movies</div>} />
         <Route path='/series' element={<div>Series</div>} />
         <Route path='/books' element={<div>Books</div>} />
         <Route path='/video-games' element={<div>Video games</div>} />
         <Route path='/contact' element={<div>Contact</div>} />
         <Route path='/about' element={<div>About</div>} />
-        <Route path='/register' element={<div>Register</div>} />
       </Routes>
     </div>
   );
