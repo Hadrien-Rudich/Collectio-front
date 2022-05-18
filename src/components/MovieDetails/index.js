6aa87719ab4c921492cd0a60beab8426595961d1
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2022-05-18 15:59:44 +0200

    movieDetails page done

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -45,50 +45,59 @@ function MovieDetails() {
     <div className="mediaDetails">
     {loading ? (
       <div>Chargement...</div>
-    ) : (
-      <div>
-        <img src={`https://image.tmdb.org/t/p/original/${movieResults.movie.poster_path}`} alt="" />
-        <h2 className="mediaDetails__mediaReleaseYear">{movieResults.movie.release_date}</h2>
-        <h2 className="mediaDetails__mediaRunTime">{movieResults.movie.runtime} minutes</h2>
-        <h2 className="mediaDetails__mediaTitle">{movieResults.movie.original_title}</h2>
-
-        <br />
-        <br />
-        <h3 className="mediaDetails__mediaCast">Director{movieResults.cast.crew.filter((crew) => crew.department === "Directing").length > 1 ? 's' : ''}</h3>
-        <br />
-        {movieResults.cast.crew.filter((crew) => crew.department === "Directing").slice(0, 5).map((crew) => (
-          <h4 key={crew.id} className="mediaDetails__mediaGenre">{crew.name}</h4>
-        ))}
-
-        <br />
-        <br />
-        <h3 className="mediaDetails__mediaCast">Main cast</h3>
-        <br />
-        {movieResults.cast.cast.slice(0, 5).map((cast) => (
-          <div key={cast.id}>
-            <h4 className="mediaDetails__mediaGenre">{cast.name}</h4>
-            <span>{cast.character}</span>
+    ) : (    
+      <div className="mediaContainer">
+        <div className="mediaImageContainer">          
+          <h1 className="mediaDetails__mediaTitle">{movieResults.movie.original_title}</h1>
+          <br />
+          <img src={`https://image.tmdb.org/t/p/original/${movieResults.movie.poster_path}`} alt="" />
+          <br />
+          <h2 className="mediaDetails__mediaReleaseYear">({movieResults.movie.release_date.substring(0,4)})</h2>
+          <br />
+          <h2 className="mediaDetails__mediaRunTime">{movieResults.movie.runtime} minutes</h2>
+          <br />
+          {movieResults.movie.genres.map((genre) => (
+          <h4 key={genre.id} className="mediaDetails__mediaGenre">{genre.name}</h4>          
+          ))}
+          
+        </div>
+        <div className="mediaTextContainer">                      
+          <br />
+          <div className="mediaCrewContainer">
+            <h3 className="mediaDetails__mediaCrew">Director{movieResults.cast.crew.filter((crew) => crew.department === "Directing").length > 1 ? 's' : ''}</h3>
             <br />
-            <br />
+              {movieResults.cast.crew.filter((crew) => crew.department === "Directing").slice(0, 5).map((crew) => (
+                <h4 key={crew.id} className="mediaDetails__mediaGenre">{crew.name}</h4>
+              ))}
           </div>
-        ))}
+          <br />
+          <br />
+          <div className="mediaCastContainer">
+            <h3 className="mediaDetails__mediaCast">Main cast</h3>
+            <br />
+              {movieResults.cast.cast.slice(0, 5).map((cast) => (
+              <div key={cast.id}>
+                <h4 className="mediaDetails__mediaGenre">{cast.name} ({cast.character})</h4>        
+                
+              </div>
+            
+          
+            ))}
+            </div>
 
-        <br />
-        <br />
-        <h3 className="mediaDetails__mediaOverview">Overview</h3>
-        <br />
-        <p className="mediaDetails__mediaOverview">{movieResults.movie.overview}</p>
+          <br />
+          <br />
+          <div className="mediaOverviewContainer">
+            <h3 className="mediaDetails__mediaCast">Overview</h3>
+            <br />
+            <h4 className="mediaDetails__mediaOverview">{movieResults.movie.overview}</h4>
+                            
+        </div>
 
-        <br />
-        <br />
-        <h3 className="mediaDetails__mediaOverview">Genres</h3>
-        <br />
-        {movieResults.movie.genres.map((genre) => (
-          <h4 key={genre.id} className="mediaDetails__mediaGenre">{genre.name}</h4>
-        ))}
       </div>
-    )}
     </div>
+    )}
+  </div>
   );
 }
 
