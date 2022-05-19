48efdc46f1ef8e795d3dabd460f176f91c354877
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2022-05-19 15:48:07 +0200

    movie, series, game detailed pages done, book WIP

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -42,44 +42,48 @@ function BookDetails() {
     <div className="mediaDetails">
     {loading ? (
       <div>Chargement...</div>
-    ) : (
-      <div>
-        {typeof bookResult.volumeInfo.imageLinks !== 'undefined' && (
-          <div>
-            <img src={bookResult.volumeInfo.imageLinks.thumbnail} alt="" />
-          </div>
-        )}
-        <br />
-        <br />
-        <h2 className="mediaDetails__mediaReleaseYear">PublishedDate : {bookResult.volumeInfo.publishedDate}</h2>
-        <h2 className="mediaDetails__mediaRunTime">PageCount : {bookResult.volumeInfo.pageCount}</h2>
-        <br />
-        <h2 className="mediaDetails__mediaTitle">Title : {bookResult.volumeInfo.title}</h2>
-        <h2 className="mediaDetails__mediaTitle">Subtitle : {bookResult.volumeInfo.subtitle}</h2>
-        <br />
-        <h2 className="mediaDetails__mediaTitle">Publisher : {bookResult.volumeInfo.publisher}</h2>
+    ) : (      
 
-        <br />
-        <br />
-        <h3 className="mediaDetails__mediaCast">Description</h3>
-        <br />
-        <div dangerouslySetInnerHTML={{__html: bookResult.volumeInfo.description}} />
-
-        <br />
-        <br />
-        {typeof bookResult.volumeInfo.authors !== 'undefined' && (
-          <div>
-            <h3 className="mediaDetails__mediaCast">Author{bookResult.volumeInfo.authors.length > 1 ? 's' : ''}</h3>
-            <br />
-            {bookResult.volumeInfo.authors.map((author) => (
-              <h4 key={author} className="mediaDetails__mediaTitle">{author}</h4>
-            ))}
-          </div>
-        )}
-
-        <br />
-        <br />
-        <h3 className="mediaDetails__mediaCast">Categories : {bookResult.volumeInfo.categories}</h3>
+        <div className="mediaContainer">
+          <div className="mediaImageContainer">             
+            <h1 className="mediaDetails__mediaTitle">{bookResult.volumeInfo.title}</h1>
+          
+          {typeof bookResult.volumeInfo.imageLinks !== 'undefined' && (
+          <img src={bookResult.volumeInfo.imageLinks.thumbnail} alt="" />         
+          )}
+           
+            <h2 className="mediaDetails__mediaReleaseYear">({bookResult.volumeInfo.publishedDate.substring(0,4)})</h2>
+            <h2 className="mediaDetails__mediaRunTime">{bookResult.volumeInfo.pageCount} pages</h2>
+            <div className='mediaDetails__mediaGenreContainer'>
+              <h4 className="mediaDetails__mediaGenre">{bookResult.volumeInfo.categories}</h4>
+          </div>                       
+          <div className="mediaTextContainer"> 
+           <div className="mediaCrewContainer">
+            {typeof bookResult.volumeInfo.authors !== 'undefined' && (
+              <div>
+                <h3 className="mediaDetails__mediaCrew">Author{bookResult.volumeInfo.authors.length > 1 ? 's' : ''}</h3>
+                <br />
+                {bookResult.volumeInfo.authors.map((author) => (
+                  <h4 key={author} className="mediaDetails__mediaTitle">{author}</h4>
+                ))}                
+                
+              </div>              
+              
+            )}
+              </div>                   
+                                     
+            <h2 className="mediaDetails__mediaTitle">Publisher : {bookResult.volumeInfo.publisher}</h2>
+           
+            </div>
+            
+            <div className="mediaOverviewContainer">        
+              <h4 className="mediaDetails__mediaCast" dangerouslySetInnerHTML={{__html: bookResult.volumeInfo.description}} />
+            </div>
+                                 
+           
+               </div>
+  
+        
       </div>
     )}
     </div>
